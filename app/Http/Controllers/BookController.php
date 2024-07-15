e6c4bd6d61be5fe5a812a61632ab36f0f1b52896
Javiera Rahmadhany <kazuha004@gmail.com>
2024-07-15 15:48:52 +0800

    caching

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -22,7 +22,8 @@ class BookController extends Controller
             'highest_rated_last_6months' => $books->highestRatedLast6Months(),
             default => $books->latest()
         };
-        $books = $books->get();
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books = cache()->remember($cacheKey, 3600, fn() => $books->get());
         return view('books.index', ['books' => $books]);
     }
 
@@ -47,11 +48,11 @@ class BookController extends Controller
      */
     public function show(Book $book)
     {
-        return view('books.show', [
-            'book' => $book->load([
-                'reviews' => fn($query) => $query->latest()
-            ])
-        ]);
+        $cacheKey = 'book:' . $book->id;
+        $book = cache()->remember($cacheKey, 3600, fn() => $book->load([
+            'reviews' => fn($query) => $query->latest()
+        ]));
+        return view('books.show', ['book' => $book]);
     }
 
     /**
